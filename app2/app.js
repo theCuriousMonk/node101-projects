c2af9cd96f15bb26c36a96b1fc691e0fb0169996
theCuriousMonk <rajanshu.ujjwal@gmail.com>
2017-05-21 15:27:32 +0530

    Added NodeMailer, Route

--- app2/app.js
+++ app2/app.js
@@ -1,10 +1,11 @@
+'use strict'
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
 
 var index = require('./routes/index');
 var about = require('./routes/about');
--- app2/routes/contact.js
+++ app2/routes/contact.js
@@ -1,9 +1,40 @@
+'use strict'
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact');
+});
+
+router.post('/send', function (req, res, next) {
+	let transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth : {
+			user : 'YOUR_EMAIL',
+			pass : 'YOUR_PASSWORD'
+		}
+	});
+
+
+	let mailOptions = {
+		from : 'info@theweb.com',
+		to : 'rajanshu.ujjwal@gmail.com',
+		subject : 'Mail Test from Nodemailer',
+		text : 'You have a new submission from Nodemailer with following details ' + req.body.name + ' Email ' + req.body.email + ' Message ' + req.body.message,
+		html : '<p>You got a new submission with following details..</p> <ul><li>Name : ' + req.body.name + '</li><li>Email : ' +req.body.email+ '</li><li>Message : '+req.body.message+'</li></ul>'
+	}
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('message sent ' + info.response);
+			res.redirect('/');
+		}
+	});
 });
 
 module.exports = router;
